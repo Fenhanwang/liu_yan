bd87b9494d86806c34238122cc075e5799234c76
fenhan <wfenhan@gmail.com>
2016-05-15 17:05:48 -0400

    set up angularJS, bower, ui-router, angularJS material design, augular-rails-templates, scss gem and so on; Add the index page; add ui router path and controller; edit rails router and controller;

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -2,11 +2,34 @@
 app = angular.module('app', [
 	// 'restangular',
 	'ngAnimate',
-	// 'ui.router',
+	'ui.router',
 	'templates',
 	'ngAria',
-	'ngMaterial', 
+	'ngMaterial'
 	// 'Devise',
   	// 'angularMoment',
   	// 'md.data.table'
 ])
+
+// BUG! Cannot click back button to get previous page By Fenhan May 15
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+	.state('home', {
+		url: "/home",
+		templateUrl: 'index.html'
+  	})
+  	.state('post_gossip', {
+  		url: "/post_gossip",
+  		templateUrl: 'post_gossip.html'
+  	});
+  	$urlRouterProvider.otherwise('home');  
+});
+
+// modify the customise bwTheme
+app.config(function($mdThemingProvider) {
+	// Configure a dark theme with primary foreground yellow
+    $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();	
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require angular
+//= require angular-ui-router
+//= require angular-rails-templates
+//= require angular-material
+//= require angular-animate
+//= require angular-aria
+
+//= require_tree ./angular
+//= require_tree ./templates
 //= require_tree .
